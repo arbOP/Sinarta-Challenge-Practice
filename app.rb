7d79b143a30b847618352220df8508cb5de5fe84
arbOP <newarbnor@hotmail.com>
2021-01-06 15:30:32 +0000

    new files added

--- app.rb
+++ app.rb
@@ -12,7 +12,17 @@ get '/pasta' do
   "chicken alfredo"
 end
 
-get '/cat' do
-  @names = ["Amigo", "Oscar", "Viking"].sample
+get '/random-cat' do
+  @name = ["Amigo", "Oscar", "Viking"].sample
   erb(:index)
+end
+
+post '/named-cat' do
+  p params
+  @name = params[:name]
+  erb(:index)
+end
+
+get '/form' do
+  erb(:form)
 end
